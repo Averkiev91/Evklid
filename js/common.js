4a95b6cdc7ee7ed601ed8ef36df5cd438b13eef6
Averkiev91 <amd3500m@gmail.com>
2022-06-21 14:16:15 +0300

    section work

--- js/common.js
+++ js/common.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+	/* Slider */
+
 	const swiper = new Swiper(".mySwiper", {
 		pagination: {
 			el: ".swiper-pagination",
@@ -6,5 +9,29 @@ document.addEventListener("DOMContentLoaded", () => {
 		mousewheel: {
     	invert: true,
 		},
+	})
+
+	/* work-tabs */
+
+	document.querySelectorAll('.work__btn').forEach((el) => {
+		el.addEventListener('click', (el) => {
+			const tab = el.currentTarget.dataset.path
+
+			document.querySelectorAll('.tab-content').forEach((el) => {
+				el.classList.remove('tab-content--active')
+
+				document.querySelector(`[data-target='${tab}']`).classList.add('tab-content--active')
+			})
+		})
+
+		el.addEventListener('click', (el) => {
+			const tabDefault = el.currentTarget.dataset.default
+
+			document.querySelectorAll('.work__btn').forEach((el) => {
+				el.classList.remove('work__btn--active')
+
+				document.querySelector(`[data-path='${tabDefault}']`).classList.add('work__btn--active')
+			})
+		})
 	})
 })
